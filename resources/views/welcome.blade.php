2e3e6dbf6904ce3ca9dc66ac5168b6d334ae7890
Joshua Mclean <86990609+JoshuaMc1@users.noreply.github.com>
2023-08-20 03:11:31 -0600

    On branch dev Your branch is up to date with 'origin/dev'.
    Changes to be committed:
    	modified:   package-lock.json
    	modified:   package.json
    	new file:   public/favicon.ico
    	modified:   public/js/app.js
    	new file:   public/robots.txt
    	modified:   resources/views/welcome.blade.php
    	modified:   webpack.mix.js

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,6 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="manifest" href="{{ asset('manifest.json') }}">
     <title>Bienvenido a JMFramework</title>
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
